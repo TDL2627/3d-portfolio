51754f99f4798a6e1a79c8c93acc5b5de1129b75
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-03-16 00:07:58 +0200

    finished nav bar for now

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import {
   Navbar,
   Tech,
   Works,
+  StarsCanvas,
 } from "./components";
 const App = () => {
   return (
@@ -15,8 +16,16 @@ const App = () => {
       <div className="bg-primary relative z-0">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
-    
           <Hero />
+          <About/>
+          <Experience/>
+          <Tech/>
+          <Works/>
+          <Feedbacks/>
+          <div className="relative z-0">
+           <Contact/>
+           <StarsCanvas/>
+          </div>
         </div>
       </div>
     </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,97 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../style";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 top-0 z-20 bg-primary   `}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2 text-red-500"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Kannemeyer{" "}
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white underline" : "text-secondary"
+              } font-medium cursor-pointer`}
+            >
+              <Link
+                onClick={() => {
+                  setActive(link.title);
+                }}
+                to={`#${link.id}`}
+              >
+                {link.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            onClick={() => {
+              setToggle(!toggle);
+            }}
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+          />
 
-export default Navbar
+          <div
+            className={`flex fixed right-0  top-0 bottom-0 w-1/2  h-full p-6 black-gradient z-20 transition-all duration-300  ${
+              toggle ? "" : "translate-x-full"
+            }`}
+          >
+            <img
+              onClick={() => {
+                setToggle(false);
+              }}
+              src={close}
+              alt="menu"
+              className="w-[28px] h-[28px] object-contain cursor-pointer absolute right-10"
+            />
+            <ul className="list-none  ">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white underline" : "text-secondary"
+                  } font-bold m-10 cursor-pointer font-poppins text-[16px]`}
+                >
+                  <Link
+                    onClick={() => {
+                      setActive(link.title);
+                      setToggle(false);
+                    }}
+                    to={`#${link.id}`}
+                  >
+                    {link.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
