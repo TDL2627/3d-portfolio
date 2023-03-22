660c9b2817ff8ddafaebec1a91966670e845326f
Ashley Kannemeyer <60740073+TDL2627@users.noreply.github.com>
2023-03-22 21:30:52 +0200

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,13 +13,22 @@ const Hero = () => {
             <div className="w-5 h-5 rounded-full bg-[#1d4ed8]" />
             <div className="w-1 h-96 md:h-40 blue-gradient" />
           </div>
-          <div>
-            <h1 className={` ${styles.heroHeadText}`}>
+          <div className="mt-5">
+            <h1 className={`md:text-5xl font-black md:block hidden`}>
               Hi, I'm a{" "}
-              <span className="text-[#1d4ed8]">Full Stack Web Developer</span>{" "}
+              <span className="text-[#1d4ed8] ">Full Stack Web Developer</span>{" "}
             </h1>
+            <h2 className={` text-2xl font-black md:hidden block`}>
+              Hi, I'm a{" "}
+              <span className="text-[#1d4ed8] ">Full Stack</span>{" "}
+
+            </h2>
+            <p className="text-[#1d4ed8] font-black w-full  text-2xl ">Web Developer</p>{" "}
+            <div className="mt-10">
             <p>I make websites and games</p>
             <p>I can do anything</p>
+            </div>
+           
           </div>
         </div>
       </section>
